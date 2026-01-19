215b7e876abbc01e2c3b25a0bb49bc8b3a1556b9
LukasReckwald <lukas.reckwald@gmail.com>
2026-01-19 13:31:13 +0100

    testing models

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
-function setup() {
-    createCanvas(400, 400);
+let maria;
+let metropolis;
+
+async function setup() {
+    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+    colorMode(HSB);
+
+    maria = await loadModel('models/metropolis_woman.obj', true);
+    metropolis = await loadModel('models/Metropolis.obj', true);
 }
 
 function draw() {
-    background(220);
+    background(0, 0, 50);
+
+    ambientLight(60);
+    pointLight(255, 255, 255, 0, -200, 200);
+
+    orbitControl();
+
+    scale(10);
+
+    if (maria) {
+        noStroke();
+        model(maria);
+    }
 }
